9d5e9023e2f568307363a6b0f37f2cd3705a77e9
NealHumphrey <humphrey.neal@gmail.com>
2018-02-02 23:58:03 -0500

    Established additional core functionality:
    - Search bar executes the Giphy API search and returns proper results
    - Results are displayed on the page
    - Hovering a gif toggles the image to be the animated vs. still versions
    - Clicking a gif adds it to the 'selected' array, which is also displayed above.
    
    Switched over to Bootstrap b/c the semantic ui grid system was hard to deal with (I hadn't used it before - more familiar with Bootstrap but the semantic ui default styling is a bit cleaner).
    
    Tentative idea list of features still to add:
    - Link up the 'search' button to trigger the right search parameters (currently doesn't work)
    - Add a clear search option
    - Add a remove from favorites option
    - Add a clear favorites option
    - Add a preview screen that shows a larger version of the currently toggled gif
    - Use left/right arrows to change which of the search results is currently toggled
    - Use up arrow to add to favorites
    - Use down arrow to remove from results
    - Lots of styling to make it pretty and useable

--- GiphySorter.js
+++ GiphySorter.js
@@ -1,11 +1,69 @@
 var vm = new Vue({
     el: '#giphysorter',
     data:{
-        selected: ["img1","img2"]
+        selected: [],
+        searchResults: [],
+        searchResultUrls: [],
+        api_key: 'ZZPUvuQl7b1RGfjQ4Q6adTmfj92fQd50',
+        searchUrl: 'https://api.giphy.com/v1/gifs/search',
+        imgVersion: 'fixed_width_still',
+        imgHoverVersion: 'fixed_width'
     },
     methods:{
-        addItem: function() {
-            this.selected.push('img')
+        addToSelection: function(img,event){
+            this.selected.push(img);
+            console.log(event);
+        },
+        toggleHover: function(img,event,isHovered){
+            var imgType = (isHovered == 1) ? this.imgHoverVersion : this.imgVersion;
+            event.target.src = img.images[imgType].url
+        },
+        /**
+        * Queries the Giphy API search endpoint. Upon successful 
+        * result, the data.searchResult array is updated with the
+        * results of the search.
+        */
+        search: function(event){
+            var searchTerms = event.target.value;
+
+            $.ajax({
+              dataType: "json",
+              type: "get",
+              url: this.searchUrl,
+              data: {
+                api_key:this.api_key,
+                q: searchTerms,
+                limit: 10,
+                rating:"pg"
+              },
+              success: function(response){
+                vm.searchResults = response.data;
+                console.log(vm.searchResults);
+
+                //TODO put this into a Watch instead
+                vm.extractUrls(vm.imgVersion);
+              },
+              error: function(){
+                //TODO real error handling
+                console.log("there was an error");
+              }
+            });
+        },
+        /**
+        * DEPRECATED - remove if not used later
+        * Converts the current contents of data.searchResults
+        * and extracts the url based on which image version
+        * is desired. searchResults returned from the giphy  
+        * API contain numerous image versions, stored in the form 
+        * result.images.<imageVersion>.url
+        */
+        extractUrls: function(imgVersion){
+            var result = [];
+            for (var i = 0; i < this.searchResults.length; i++){
+                var img = this.searchResults[i].images[imgVersion]
+                result.push(img.url)
+            };
+            this.searchResultUrls = result;
         }
     }
 });
